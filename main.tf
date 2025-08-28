54cc334125370ac132728bcdbfe1f71823788646
vardhan-930 <serioneplus@outlook.com>
2025-08-28 18:16:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers { aws = { source = "hashicorp/aws", version = "~> 5.0" } }
 }
+provider "aws" { region = "us-east-1" }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-public-bucket"
-  acl    = "public-read"   # ❌ Security risk: public bucket
+resource "aws_s3_bucket" "bad" {
+  bucket = "my-iac-demo-bucket-123456"
+  # Missing encryption/public access block to trigger Checkov
 }
